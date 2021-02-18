8623d73b793b4bce647b12203892242307eb1c02
Shakeel Osmani <shakeel.osmani@gmail.com>
2021-02-18 14:16:42 -0500

    Update quasar.conf.js
    forcing build to be from build

--- quasar.conf.js
+++ quasar.conf.js
@@ -57,10 +57,10 @@ module.exports = function (/* ctx */) {
       // showProgress: false,
       // gzip: true,
       // analyze: true,
-
+      distDir: "build",
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
+      
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
       },
